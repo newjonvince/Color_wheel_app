800bcd0edad4eea5ccfda9f541ab95dd907f31e4
newjonvince <JnVincentt@gmail.com>
2025-11-14 11:03:56 -0500

    HOTFIX: Temporarily disable path aliases to fix bundling error
    - Comment out babel-plugin-module-resolver configuration in babel.config.js
    - Comment out Metro path aliases in metro.config.js
    - Fix 'Cannot find module babel-plugin-module-resolver' error in Expo bundling
    - Keep configuration commented for future re-enabling when properly set up
    
    Issue: Expo cloud build environment can't find babel-plugin-module-resolver
    Solution: Disable path aliases temporarily, use relative imports for now

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,20 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 // Disable New Architecture to prevent Swift compilation errors
 config.resolver.unstable_enablePackageExports = false;
 
-// Add path aliases for cleaner imports (must match babel.config.js)
-config.resolver.alias = {
-  '@': path.resolve(__dirname, 'src'),
-  '@components': path.resolve(__dirname, 'src/components'),
-  '@screens': path.resolve(__dirname, 'src/screens'),
-  '@services': path.resolve(__dirname, 'src/services'),
-  '@utils': path.resolve(__dirname, 'src/utils'),
-  '@hooks': path.resolve(__dirname, 'src/hooks'),
-  '@config': path.resolve(__dirname, 'src/config'),
-  '@assets': path.resolve(__dirname, 'assets'),
-};
+// Path aliases temporarily disabled - need to install babel-plugin-module-resolver properly
+// config.resolver.alias = {
+//   '@': path.resolve(__dirname, 'src'),
+//   '@components': path.resolve(__dirname, 'src/components'),
+//   '@screens': path.resolve(__dirname, 'src/screens'),
+//   '@services': path.resolve(__dirname, 'src/services'),
+//   '@utils': path.resolve(__dirname, 'src/utils'),
+//   '@hooks': path.resolve(__dirname, 'src/hooks'),
+//   '@config': path.resolve(__dirname, 'src/config'),
+//   '@assets': path.resolve(__dirname, 'assets'),
+// };
 
 module.exports = config;
